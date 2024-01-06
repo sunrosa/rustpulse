7d393de8d4e0b8c6c01e7589e63e54bbc116e225
Ibis Sunrosa <drsunrosa@gmail.com>
2024-01-06 14:44:06 +0000

    Key from input refactor

--- src/query.rs
+++ src/query.rs
@@ -60,6 +60,19 @@ async fn each_keypresses(db_path: &str) {
 async fn specific_keypresses(db_path: &str) {
     let mut db = db::initialize_db(db_path).await;
 
+    let key = key_from_input();
+
+    let presses: i64 = sqlx::query("SELECT COUNT(*) as count FROM keypresses WHERE key == ?")
+        .bind(u64::from(key) as i64)
+        .fetch_one(&mut db)
+        .await
+        .unwrap()
+        .get(0);
+
+    println!("{:?}: {}", key, presses);
+}
+
+fn key_from_input() -> KeybdKey {
     let selection = Select::new(
         "Key",
         vec![
@@ -68,7 +81,7 @@ async fn specific_keypresses(db_path: &str) {
             "4", "$", "5", "%", "6", "^", "7", "&", "8", "*", "9", "(", "`", "~", "/", "?", ",",
             ".", "-", "_", ";", ":", "[", "{", "]", "}", "=", "+", "\\", "|", "'", "\"", "F1",
             "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "SPACE", "CAPS",
-            "LSHIFT", "RSHIFT", "LCTRL", "RCTRL", "LALT", "RALT", "LSUPER", "RSUPER", "ESC"
+            "LSHIFT", "RSHIFT", "LCTRL", "RCTRL", "LALT", "RALT", "LSUPER", "RSUPER", "ESC",
         ],
     )
     .prompt()
@@ -83,41 +96,33 @@ async fn specific_keypresses(db_path: &str) {
     if selection_char.is_some() {
         key = inputbot::get_keybd_key(selection_char.unwrap());
     }
-    if key.is_none() {
-        key = Some(match selection {
-            "F1" => KeybdKey::F1Key,
-            "F2" => KeybdKey::F2Key,
-            "F3" => KeybdKey::F3Key,
-            "F4" => KeybdKey::F4Key,
-            "F5" => KeybdKey::F5Key,
-            "F6" => KeybdKey::F6Key,
-            "F7" => KeybdKey::F7Key,
-            "F8" => KeybdKey::F8Key,
-            "F9" => KeybdKey::F9Key,
-            "F10" => KeybdKey::F10Key,
-            "F11" => KeybdKey::F11Key,
-            "F12" => KeybdKey::F12Key,
-            "SPACE" => KeybdKey::SpaceKey,
-            "CAPS" => KeybdKey::CapsLockKey,
-            "LSHIFT" => KeybdKey::LShiftKey,
-            "RSHIFT" => KeybdKey::RShiftKey,
-            "LCTRL" => KeybdKey::LControlKey,
-            "RCTRL" => KeybdKey::RControlKey,
-            "LALT" => KeybdKey::LAltKey,
-            "RALT" => KeybdKey::RAltKey,
-            "LSUPER" => KeybdKey::LSuper,
-            "RSUPER" => KeybdKey::RSuper,
-            "ESC" => KeybdKey::EscapeKey,
-            _ => unreachable!(),
-        });
-    }
 
-    let presses: i64 = sqlx::query("SELECT COUNT(*) as count FROM keypresses WHERE key == ?")
-        .bind(u64::from(key.unwrap()) as i64)
-        .fetch_one(&mut db)
-        .await
-        .unwrap()
-        .get(0);
+    let key = match selection {
+        "F1" => KeybdKey::F1Key,
+        "F2" => KeybdKey::F2Key,
+        "F3" => KeybdKey::F3Key,
+        "F4" => KeybdKey::F4Key,
+        "F5" => KeybdKey::F5Key,
+        "F6" => KeybdKey::F6Key,
+        "F7" => KeybdKey::F7Key,
+        "F8" => KeybdKey::F8Key,
+        "F9" => KeybdKey::F9Key,
+        "F10" => KeybdKey::F10Key,
+        "F11" => KeybdKey::F11Key,
+        "F12" => KeybdKey::F12Key,
+        "SPACE" => KeybdKey::SpaceKey,
+        "CAPS" => KeybdKey::CapsLockKey,
+        "LSHIFT" => KeybdKey::LShiftKey,
+        "RSHIFT" => KeybdKey::RShiftKey,
+        "LCTRL" => KeybdKey::LControlKey,
+        "RCTRL" => KeybdKey::RControlKey,
+        "LALT" => KeybdKey::LAltKey,
+        "RALT" => KeybdKey::RAltKey,
+        "LSUPER" => KeybdKey::LSuper,
+        "RSUPER" => KeybdKey::RSuper,
+        "ESC" => KeybdKey::EscapeKey,
+        _ => key.unwrap(),
+    };
 
-    println!("{:?}: {}", key.unwrap(), presses);
+    key
 }
