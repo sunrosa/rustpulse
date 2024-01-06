e021a5c473424d2d430b313c9d4b61cd585b8938
Ibis Sunrosa <drsunrosa@gmail.com>
2024-01-06 06:20:33 +0000

    Prepare for database integration

--- src/main.rs
+++ src/main.rs
@@ -14,14 +14,14 @@ fn main() {
     thread::Builder::new()
         .name("Debugger".into())
         .spawn(|| loop {
-            thread::sleep(Duration::from_secs(60));
+            thread::sleep(Duration::from_secs(20));
 
             let mut output = String::new();
             for key in stats::get_keypresses().lock().unwrap().deref() {
-                output += &format!("{}, {:?}\n", key.0.to_rfc3339(), key.1);
+                output += &format!("{},{}\n", key.0.timestamp(), u64::from(key.1));
             }
 
-            println!("{output}");
+            print!("{output}");
 
             stats::reset_keypresses();
         })
