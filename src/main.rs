be0fee9432241a6ed4f3b70296f93ebe598a86f7
Ibis Sunrosa <drsunrosa@gmail.com>
2024-01-06 08:26:26 +0000

    It works?

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,48 @@
 mod stats;
 
-use std::{
-    ops::{Deref, DerefMut},
-    thread,
-    time::Duration,
-};
+use std::{ops::Deref, thread, time::Duration};
 
 use inputbot::{handle_input_events, KeybdKey};
+use sqlx::{migrate::MigrateDatabase, Sqlite, SqlitePool};
 
-fn main() {
+#[tokio::main]
+async fn main() {
     register_bindings();
 
-    thread::Builder::new()
-        .name("Debugger".into())
-        .spawn(|| loop {
-            thread::sleep(Duration::from_secs(20));
+    tokio::task::spawn(async {
+        let db_path = "events.db";
 
-            let mut output = String::new();
-            for key in stats::get_keypresses().lock().unwrap().deref() {
-                output += &format!("{},{}\n", key.0.timestamp(), u64::from(key.1));
+        if !Sqlite::database_exists(db_path).await.unwrap_or(false) {
+            println!("Creating database at {db_path}...");
+            match Sqlite::create_database(db_path).await {
+                Ok(_) => println!("Success creating database."),
+                Err(error) => panic!("Error creating database: {}", error),
             }
+        } else {
+            println!("Database already exists at {db_path}.");
+        }
 
-            print!("{output}");
+        let db = SqlitePool::connect(db_path).await.unwrap();
+        sqlx::query("CREATE TABLE IF NOT EXISTS keypresses (id INTEGER PRIMARY KEY NOT NULL, timestamp INTEGER NOT NULL, key INTEGER NOT NULL);").execute(&db).await.unwrap();
 
-            stats::reset_keypresses();
-        })
-        .unwrap();
+        loop {
+            tokio::time::sleep(Duration::from_secs(20)).await;
+
+            let transaction = db.begin().await.unwrap();
+            for key in stats::get_keypresses().lock().await.deref() {
+                println!("{:?}", key);
+                sqlx::query("INSERT INTO keypresses (timestamp, key) VALUES (?, ?);")
+                    .bind(key.0.timestamp())
+                    .bind(u64::from(key.1) as i64)
+                    .execute(&db)
+                    .await
+                    .unwrap();
+            }
+            transaction.commit().await.unwrap();
+
+            stats::reset_keypresses().await;
+        }
+    });
 
     handle_input_events();
 }
@@ -35,5 +52,5 @@ fn register_bindings() {
 }
 
 fn keypress_handler(key: KeybdKey) {
-    stats::add_keypress(key);
+    stats::add_keypress_blocking(key);
 }
--- src/stats.rs
+++ src/stats.rs
@@ -1,22 +1,24 @@
-use std::{
-    ops::Deref,
-    sync::{Mutex, MutexGuard},
-};
+use std::ops::Deref;
 
 use chrono::{DateTime, Utc};
 use inputbot::KeybdKey;
 use once_cell::sync::Lazy;
+use tokio::sync::Mutex;
 
 static PRESSES: Lazy<Mutex<Vec<(DateTime<Utc>, KeybdKey)>>> = Lazy::new(|| Mutex::new(Vec::new()));
 
-pub fn add_keypress(key: KeybdKey) {
-    PRESSES.lock().unwrap().push((Utc::now(), key));
+pub async fn add_keypress(key: KeybdKey) {
+    PRESSES.lock().await.push((Utc::now(), key));
+}
+
+pub fn add_keypress_blocking(key: KeybdKey) {
+    PRESSES.blocking_lock().push((Utc::now(), key));
 }
 
 pub fn get_keypresses<'a>() -> &'a Mutex<Vec<(DateTime<Utc>, KeybdKey)>> {
     PRESSES.deref()
 }
 
-pub fn reset_keypresses() {
-    *PRESSES.lock().unwrap() = Vec::new();
+pub async fn reset_keypresses() {
+    *PRESSES.lock().await = Vec::new();
 }
